9d4795cdeb8541444ce37bd0921bcf5c79fc5874
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-06-24 22:09:14 +0530

    learnt updation and deletion

--- src/main/java/org/ashutosh/App.java
+++ src/main/java/org/ashutosh/App.java
@@ -34,6 +34,35 @@ public class App
                 System.out.println(e.getMessage());
             }
 
+            updateData(connection, 5, "AshutoshPritha", "6384137446", "ashutosh.dev18@gmail.com");
+            try{
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM customer WHERE id = 5");
+                while(resultSet.next()){
+                    System.out.println("---------UPDATED DATA IS AS FOLLOW--------------");
+                    System.out.println("ID: "+resultSet.getInt("id")
+                    +" NAME: "+resultSet.getString("name")
+                    +" PHONE NUMBER: "+resultSet.getString("phone_number")
+                    +" EMAIL ID: "+resultSet.getString("email_id"));
+                }
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+            deleteData(connection, 1, "Pritha");
+            System.out.println("------------- DATA AFTER DELETING PRITHA -------------------");
+            try{
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM customer");
+                while(resultSet.next()){
+                    System.out.println("---------UPDATED DATA IS AS FOLLOW--------------");
+                    System.out.println("ID: "+resultSet.getInt("id")
+                            +" NAME: "+resultSet.getString("name")
+                            +" PHONE NUMBER: "+resultSet.getString("phone_number")
+                            +" EMAIL ID: "+resultSet.getString("email_id"));
+                }
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
         }catch(SQLException e){
             System.out.println("Failed to connect to DB!!!!");
         }
@@ -51,6 +80,32 @@ public class App
             System.out.println(rowsAffected+" inserted into db");
         }catch(SQLException e){
             e.printStackTrace();
+        }
+    }
+
+    private static void updateData(Connection connection, int id, String name, String phone_number, String email_id){
+        String updateName = "UPDATE customer SET name = ? WHERE id = ?";
+
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(updateName);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, id);
+            int rowsAffected  = preparedStatement.executeUpdate();
+            System.out.println(rowsAffected+" row(s) were updated");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void deleteData(Connection connection, int id, String name){
+        String deleteQuery = "DELETE FROM customer WHERE name = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
+            preparedStatement.setString(1, name);
+            int rowsAffected = preparedStatement.executeUpdate();
+            System.out.println(rowsAffected+" row(s) were deleted");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
         }
     }
 }
